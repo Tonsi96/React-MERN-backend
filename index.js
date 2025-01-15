11802cdcc193aec401194a48e5e47ccbc9dfd2b2
Gaston Pizarro <tonsisofy@gmail.com>
2025-01-15 17:59:13 -0400

    CalendarApp

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { dbConnection } = require('./db/config');
+const cors = require('cors');
+require('dotenv').config();
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio Publico
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD: eventos
+
+
+// Escuchar petisiones
+app.listen( process.env.PORT, () =>{
+  console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+/*
+  Rutas de Usuarios / Auth 
+  host + /api/auth
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const router = Router();
+
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+router.post('/new',
+  [ //middlewares
+    check('name', 'El nombre es necesario').not().isEmpty(),
+    check('email', 'El email es necesario').isEmail(),
+    check('password', 'La contraseña debe de tener al menos 6 digitos').isLength({ min: 6 }),
+    validarCampos
+  ]
+  ,crearUsuario);
+
+router.post('/',
+  [
+    check('email', 'El email no es correcto').isEmail(),
+    check('password', 'La contraseña no es correcta').isLength({ min: 6 }),
+    validarCampos
+  ] 
+  ,loginUsuario);
+
+router.get('/renew', validarJWT ,revalidarToken );
+
+
+module.exports = router;
